b641c0d02a3878c5a786d578f3e8e8aaab045883
Amit <amitchaurasia774@gmail.com>
2024-08-31 00:43:07 +0530

    modify

--- Array/reverse_array.cpp
+++ Array/reverse_array.cpp
@@ -1,2 +1,32 @@
 #include<iostream>
 using namespace std;
+ 
+void reverse(int *arr, int n)
+{
+    int s=0; int e=n-1;
+
+    while(s<e)
+    {
+        swap(arr[s], arr[e]);
+        s++;
+        e--;
+    }
+    for(int i=0; i<n ; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
+
+int main()
+{
+    int arr[]={2,3,4,5,6};
+    int n=sizeof(arr)/sizeof(int);
+
+    reverse(arr, n);
+    return 0;
+
+
+}
