e3960d2e976cb05dd7c0af4e89bb375afd5bec1e
Amit <amitchaurasia774@gmail.com>
2024-11-23 23:04:59 +0530

    add file

---
+++ OOPS/ojects_and _class.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class Student{
+    string name;
+    float cgpa;
+
+    void getPercentage()
+    {
+        cout<<(cgpa * 10)<<" %  \n";
+    }
+};
+
+int main()
+{
+    Student s1;
+    cout<<sizeof(s1)<<endl;
+    return 0;
+}
