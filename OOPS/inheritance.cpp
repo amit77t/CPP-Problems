741a4aa5ce03c281645470c52072945c78483c69
Amit <amitchaurasia774@gmail.com>
2024-11-30 19:52:19 +0530

    add file

---
+++ OOPS/inheritance.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+ class Animal{
+
+    public:
+    string color;
+    void eat()
+    {
+        cout<<"eats\n";
+    }
+    void breathe()
+    {
+        cout<<"breathes\n";
+    }
+
+ };
+ class Fish : public Animal{
+  
+  public:
+   int fins;
+   void swim()
+   {
+     cout<<"swims\n";
+   }
+ };
+
+
+
+int main(){
+ Fish f1;
+ f1.fins=3;
+ f1.swim();
+ f1.eat();
+ f1.breathe();
+ return 0;
+}
