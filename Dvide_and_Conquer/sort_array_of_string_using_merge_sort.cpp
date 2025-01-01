7ba563f1ca81a94e18710851a2940963a00e6249
Amit <amitchaurasia774@gmail.com>
2025-01-01 22:19:09 +0530

    add file

---
+++ Dvide_and_Conquer/sort_array_of_string_using_merge_sort.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+   string arr[4]={"sun", "earth"," mars","mercury"};
+
+   cout<<arr[0]<<endl;
+
+   return 0;
+   
+
+}
