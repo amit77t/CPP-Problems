73911ebf56746164cb2964c57c559c057396d589
Amit <amitchaurasia774@gmail.com>
2025-01-02 12:40:10 +0530

    add new problem solution

---
+++ Recursion/print_x_to_the_power_n.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int pow(int x, int n)
+{
+    if(n==0)
+    {
+        return 1;
+    }
+    int halfPow= pow(x, n/2);
+    int halfPowSquare=halfPow*halfPow;
+    if(n%2 !=0)
+    {
+        return x*halfPowSquare;
+    }
+    return halfPowSquare;
+}
+
+
+int main()
+{
+   cout<<pow(2,2)<<endl;
+   return 0;
+}
