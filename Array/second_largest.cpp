807135521d8665d2568168dff1bb0287ac527d61
Amit <amitchaurasia774@gmail.com>
2025-08-21 01:07:20 +0530

    Add implementation to find the second largest element in an array

---
+++ Array/second_largest.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int secondLargest(int arr[], int n)
+{
+   int res=-1;
+   int largest=0;
+
+   for(int i=1; i<n ; i++)
+   {
+    if(arr[i]>arr[largest])
+    {
+        res=largest;
+        largest=i;
+    }
+    else if( arr[i]!= arr[largest])
+    {
+        if(res==-1 || arr[i]> arr[res])
+        {
+            res=i;
+        }
+    }
+   }
+   return res;
+}
+
+int main()
+{
+
+    int arr[6]={1,2,4,7,8,9};
+    cout<<secondLargest(arr,6)<<endl;
+    return 0;
+}
