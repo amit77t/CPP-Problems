ee13da20c941223c59682d08506f9581fb148487
Amit <amitchaurasia774@gmail.com>
2025-09-25 23:15:04 +0530

    add implementation for finding the leftmost non-repeating character in a string

---
+++ String/leftmost_non_reapeating_char.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int leftmostNonReapeating(string &s)
+{
+    vector<int> freq(26,0);
+    for(char c: s)
+    {
+        freq[c-'a']++;
+    }
+
+   
+
+    for(int i=0; i<s.size(); i++)
+    {
+        if(freq[s[i]-'a']==1)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+
+
+
+
+
+int main()
+{
+  string s="leetcode";
+  cout<<leftmostNonReapeating(s);
+  return 0;
+} 
