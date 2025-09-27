4e33ff78a0e9adfae670e154b6e1c7e613d41572
Amit <amitchaurasia774@gmail.com>
2025-09-27 23:42:10 +0530

    add file

---
+++ BinarySearch/find_peak_element.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int peakElement(vector<int>& arr)
+{
+    int n=arr.size();
+    int low=0;
+    int hight=n-1;
+
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+
+
+    }
+}
+
+
+int main()
+{
+
+}
